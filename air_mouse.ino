7b5b3ac06eb5d2756a8a8c9963702281c0293b31
velizarstoianov <velizar.stoianov@gmail.com>
2019-03-16 11:13:11 +0200

    Update air_mouse.ino
    [Testing]Testing water sensor

--- air_mouse.ino
+++ air_mouse.ino
@@ -3,83 +3,85 @@ int inPin = 7;
 int fanstate = 0;
 int buttoncurrentstate = 0;
 int buttonprevstate = LOW;
-int tresshold = 50;
+int tresshold = 0;
 int waterlevel = A1;
+int waterSensor = 0;
+
+
 
 void setup() {
-  
-  analogRead(waterlevel);
-  pinMode(fanPin, OUTPUT);
   pinMode(inPin, INPUT_PULLUP);
+  pinMode(fanPin, OUTPUT);
   Serial.begin(9600);
 
 }
 
-void setUpFan(){
+void setUpFan() {
 
   digitalWrite(fanPin, HIGH);
   //delay(5000);
-  //digitalWrite(fanPin, LOW); 
- 
+  //digitalWrite(fanPin, LOW);
+
 }
 
 void loop() {
 
-  
- 
+
+
   /*
-  val = digitalRead(inPin);
-  if (val == HIGH){
-  digitalWrite(fanPin, LOW);
-  } else {
+    val = digitalRead(inPin);
+    if (val == HIGH){
+    digitalWrite(fanPin, LOW);
+    } else {
     digitalWrite(fanPin, HIGH);
-  }
+    }
   */
+  waterSensor = analogRead(waterlevel);
   buttonprevstate = buttoncurrentstate;
   buttoncurrentstate = digitalRead(inPin);
   waterlevel = analogRead(A1);
+ // Serial.println(waterSensor);
 
-  
-if (buttoncurrentstate == LOW && buttonprevstate == HIGH){   
-   fanstate++;
-   Serial.println((int)fanstate);
-}
+  if (buttoncurrentstate == LOW && buttonprevstate == HIGH) {
+    fanstate++;
+//    Serial.println((int)fanstate);
+  }
 
-
-switch(fanstate){
-  case 0 : 
-    digitalWrite(fanPin, LOW);
-    break;
-  case 1 : 
-    setUpFan();
-    break;//digitalWrite(fanPin, LOW); 
-  case 2 : 
-    //Serial.println(tresshold);
-    if(waterlevel > tresshold)
+  Serial.println((int)fanstate);
+  switch (fanstate) {
+    case 0 :
+      digitalWrite(fanPin, LOW);
+      break;
+    case 1 :
       setUpFan();
+      break;//digitalWrite(fanPin, LOW);
+    case 2 :
+      //Serial.println(tresshold);
+      if (waterlevel > tresshold)
+        setUpFan();
       //digitalWrite(fanPin, HIGH);
-    else
-      digitalWrite(fanPin, LOW);  
+      else
+        digitalWrite(fanPin, LOW);
 
-    break;
-  case 3 : 
+      break;
+    case 3 :
       fanstate = 0;
-      break;  
-}
+      break;
+  }
 
-/*if(fanstate == 0){
-  
-}
+  /*if(fanstate == 0){
 
-if(fanstate == 1){
-   
-}
-if(fanstate == 2){
-  
-}
+    }
 
-if(fanstate == 4){
-   
-}*/ 
-//Serial.println((int)waterlevel);
+    if(fanstate == 1){
+
+    }
+    if(fanstate == 2){
+
+    }
+
+    if(fanstate == 4){
+
+    }*/
+  //Serial.println((int)waterlevel);
 }
